d5b1bfedd47b0c9ff0664c578f6a0853d62af322
wizzniu <wizzniu@gmail.com>
2020-11-29 01:12:39 +0800

    delete serverless_v0

--- Model_GRU.py
+++ Model_GRU.py
@@ -8,36 +8,37 @@
 @version      :1.0
 '''
 
-
 import torch
 import torch.nn as nn
-# from DataLoad import TIME_STEP, BATCH_SIZE, INPUT_SIZE
 
 
-INPUT_SIZE = 11
-TIME_STEP = 5
-BATCH_SIZE = 1
-
-
-class GRU(nn.Module):
-    def __init__(self, input_size=1, hidden_size=16, num_layers=1, output_size=1):
+class MyGRU(nn.Module):
+    def __init__(self, input_size=1, hidden_size=16, num_layers=1, output_size=1, batch_size=1, drop_out=0):
         super().__init__()
         self.hidden_size = hidden_size
-        self.gru = nn.GRU(input_size, hidden_size, num_layers, dropout=0.5)
+        self.num_layers = num_layers
+        self.batch_size = batch_size
+        self.hidden_cell = torch.zeros(
+            num_layers, batch_size, hidden_size)
+        self.gru = nn.GRU(input_size, hidden_size, num_layers,
+                          batch_first=False, dropout=drop_out)
         #self.linear1 = nn.Linear(hidden_size, 3)
         #self.linear2 = nn.Linear(3, 6)
-        self.fully = nn.Linear(hidden_size, output_size)
-        self.hidden_cell = torch.zeros(num_layers, 1, self.hidden_size)
+        self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, input):
-        # input(time_step,batch_size,input_size)
-        # gru_out(time_step,batch_size,hidden_size)
-        # output(time_step,batch_size,output_size)
-        input_seq = input.view(TIME_STEP, BATCH_SIZE,
-                               INPUT_SIZE)  # reshape the input!!!
+    def forward(self, input_seq):
+        '''
+        input_seq(time_step,batch_size,input_size)
+        gru_out(time_step,batch_size,hidden_size)
+
+        '''
+        # input_seq: (batch_size,time_step,input_size) -> (time_step,batch_size,input_size)!!!
+        input_seq = input_seq.permute(1, 0, 2)
         gru_out, self.hidden_cell = self.gru(input_seq, self.hidden_cell)
-        #linear1_out = self.linear1(gru_out)
+        # 仅取timestep的最后一步, size of output：[batch_size, hidden_size]
+        output = gru_out[-1, :, :]
+        #linear1_out = self.linear1(output)
         #linear2_out = self.linear2(linear1_out)
-        output = nn.functional.relu(self.fully(gru_out))
-        # 仅取timestep的最后一步，将输出展开为一维的数组！一维数组的大小=pred_counts*pred_step
-        return torch.round(output[-1].view(-1))
+        output = self.fc(output)
+
+        return output
